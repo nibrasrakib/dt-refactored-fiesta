42f059eaa75bfcdf27526b4cd4b35fa527b6ed1d
Nibras Ar Rakib <nibras@Nibrass-MacBook-Pro.local>
2020-02-01 18:13:36 +0600

    making config

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
 var REQ = {
-    fetchReq: function(url, type, data){
+    fetchReq: function (url, type, data) {
         return $.ajax({
-            type:"POST",
+            type: type,
             data: data,
             url: url
-        }).done(function(){
-            alert("Success");
-        }).fail(function(){
-            alert("Failed")
-        }).always(function(){
-            alert("Request Finish")
+        }).done(function () {
+            console.log("Request Success");
+        }).fail(function () {
+            alert("Request Failed");
+        }).always(function () {
+            console.log("Request Finish");
         });
+    }
+};
+
+var DT = {
+    config: {},
+    getInput: function (inputType) {
+        switch (inputType, data) {
+            case inputType == 'text':
+                return '<input type="text" class="organization-type" value="' +
+                    data + '">';
+
+            default:
+                break;
+        }
+    },
+    getColumns: function () {
+        return this.config;
     }
 };
